5621efcc4d3639fc1f8cd8f137eb50aeda002333
zcs <1684391434@qq.com>
2022-01-10 19:47:32 +0800

    public

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
 # git remote add origin https://github.com/sanyuan0704/my_blog.git
-git remote add origin https:/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@github.com/yishenlaoban/my_blog.git
+git remote add origin https://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@github.com/yishenlaoban/my_blog.git
 
 git push -f origin  master:master
 # git push -f https://github.com/yishenlaoban/myBlog.git master
